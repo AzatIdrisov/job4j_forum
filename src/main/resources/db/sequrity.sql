67043cbb8170d30de9d8f88a35d4fbfa49604b13
Azat Idrisov <azzzat72@yandex.ru>
2021-08-22 21:08:01 +0500

    0. Spring Liquibase [#302330]

---
+++ src/main/resources/db/sequrity.sql
@@ -0,0 +1,15 @@
+create table authorities(
+                            id serial primary key,
+                            authority varchar(50) not null unique
+);
+
+create table users(
+                      id serial primary key,
+                      username varchar(50) not null unique,
+                      password varchar(100) not null,
+                      enabled boolean default true,
+                      authority_id int references authorities(id)
+);
+
+insert into authorities (authority) values ('USER');
+insert into authorities (authority) values ('ADMIN');
