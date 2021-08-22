e7e9e4794c20f813f56bf1a334d10f262c29ba2a
Azat Idrisov <azzzat72@yandex.ru>
2021-08-22 15:46:36 +0500

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,15 +1,23 @@
 package ru.job4j.forum.control;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -17,16 +25,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class PostControlTest {
+
+    @MockBean
+    private PostService posts;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     @WithMockUser
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void shouldReturnDefaultMessageGetMethod() throws Exception {
         this.mockMvc.perform(get("/edit"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("edit"));
     }
 
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessagePostMethod() throws Exception {
+        this.mockMvc.perform(post("/save")
+                .param("name", "Test"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("Test"));
+    }
 }
