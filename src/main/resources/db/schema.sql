22d26df325da6a312c1e9c487238f688364d693a
Azat Idrisov <azzzat72@yandex.ru>
2021-09-07 20:14:43 +0500

    update heroku prop

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -5,5 +5,13 @@ create table posts (
                        created timestamp without time zone not null default now()
 );
 
+create table messages (
+                       id serial primary key,
+                       content text,
+                       created timestamp without time zone not null default now(),
+                       posts_id  int not null references posts (id),
+                       author text
+);
+
 insert into posts (name, description) values ('О чем этот форум?', '.');
 insert into posts (name, description) values ('Правила форума.', '.');
